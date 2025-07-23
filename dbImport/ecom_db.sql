3d0be7943fc185eeaebd7f4e4a0c4272a9f25fdb
Aatish250 <machamasi321@gmail.com>
2025-07-23 12:09:23 +0545

    update DB ecom_db.sql

--- dbImport/ecom_db.sql
+++ dbImport/ecom_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2025 at 06:51 AM
+-- Generation Time: Jul 23, 2025 at 08:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -89,7 +89,7 @@ INSERT INTO `image` (`img_id`, `image`) VALUES
 
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
-  `uid` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `shipping_fee` decimal(10,0) NOT NULL,
   `total` decimal(10,2) NOT NULL,
   `status` enum('pending','processing','shipped','delivered','cancelled') DEFAULT 'pending',
@@ -108,16 +108,9 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `uid`, `shipping_fee`, `total`, `status`, `shipping_address`, `phone`, `payment_method`, `payment_status`, `khalti_token`, `notes`, `admin_notes`, `created_at`, `updated_at`) VALUES
-(1, 1, 10, 13.21, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, 'Test 1', NULL, '2025-07-23 04:34:12', NULL),
-(2, 1, 0, 153.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:43:37', NULL),
-(3, 1, 10, 13.24, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:44:40', NULL),
-(4, 1, 10, 41.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:45:25', NULL),
-(5, 1, 10, 11.62, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:45:35', NULL),
-(6, 1, 0, 1200.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:45:52', NULL),
-(7, 1, 0, 153.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:47:15', NULL),
-(8, 1, 10, 11.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:49:26', NULL),
-(9, 1, 10, 10.03, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, '', NULL, '2025-07-23 04:49:55', NULL);
+INSERT INTO `orders` (`order_id`, `user_id`, `shipping_fee`, `total`, `status`, `shipping_address`, `phone`, `payment_method`, `payment_status`, `khalti_token`, `notes`, `admin_notes`, `created_at`, `updated_at`) VALUES
+(10, 1, 0, 2553.00, 'pending', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, 'Test1', NULL, '2025-07-23 05:29:56', NULL),
+(11, 1, 0, 322.00, 'shipped', 'Bhaktapur, Suryabinayak', '9800000000', 'cod', 'pending', NULL, 'TEst 2', NULL, '2025-07-23 05:33:56', '2025-07-23 05:41:45');
 
 -- --------------------------------------------------------
 
@@ -126,6 +119,15 @@ INSERT INTO `orders` (`order_id`, `uid`, `shipping_fee`, `total`, `status`, `shi
 -- (See below for the actual view)
 --
 CREATE TABLE `order_details` (
+`user_id` int(11)
+,`order_id` int(11)
+,`order_item_id` int(11)
+,`product_id` int(11)
+,`product_name` varchar(50)
+,`image` varchar(100)
+,`order_price` decimal(10,2)
+,`quantity` int(11)
+,`sub_total` decimal(15,0)
 );
 
 -- --------------------------------------------------------
@@ -148,17 +150,11 @@ CREATE TABLE `order_items` (
 --
 
 INSERT INTO `order_items` (`order_item_id`, `order_id`, `product_id`, `quantity`, `order_price`, `sub_total`) VALUES
-(96, 1, 5, 7, 0.03, 0),
-(97, 1, 11, 3, 1.00, 3),
-(98, 2, 4, 1, 153.00, 153),
-(99, 3, 6, 2, 1.62, 3),
-(100, 4, 11, 1, 1.00, 1),
-(101, 4, 10, 1, 30.00, 30),
-(102, 5, 6, 1, 1.62, 2),
-(103, 6, 1, 1, 1200.00, 1200),
-(104, 7, 4, 1, 153.00, 153),
-(105, 8, 11, 1, 1.00, 1),
-(106, 9, 5, 1, 0.03, 0);
+(109, 10, 1, 2, 1200.00, 2400),
+(110, 10, 4, 1, 153.00, 153),
+(111, 11, 7, 3, 100.00, 300),
+(112, 11, 11, 2, 1.00, 2),
+(113, 11, 9, 2, 10.00, 20);
 
 -- --------------------------------------------------------
 
@@ -181,15 +177,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `stock`, `category`, `description`, `img_id`) VALUES
-(1, 'product name1', 1200.00, 994, 'Game', 'product', 1),
-(4, 'product name4', 153.00, 975, 'earphone', 'fadshjke', 4),
+(1, 'product name1', 1200.00, 992, 'Game', 'product', 1),
+(4, 'product name4', 153.00, 974, 'earphone', 'fadshjke', 4),
 (5, 'product name', 0.03, 930, 'laptop', 'cxz', 5),
 (6, 'product name', 1.62, 979, 'speaker', 'zX', 6),
-(7, 'Apple', 100.00, 999, 'fruit', 'Apple', 7),
-(9, 'Carrot', 10.00, 999, 'Vegitable', 'Orange', 9),
+(7, 'Apple', 100.00, 996, 'fruit', 'Apple', 7),
+(9, 'Carrot', 10.00, 997, 'Vegitable', 'Orange', 9),
 (10, 'Banana', 30.00, 998, 'Fruit', 'Banana', 10),
-(11, 'OAAAE!', 1.00, 966, 'Game', 'Ke hereko handium! ?', 11),
-(12, 'Test2', 13.00, 991, 'Category2', 'Testing categories', 12);
+(11, 'OAAAE!', 1.00, 962, 'Game', 'Ke hereko handium! ?', 11),
+(12, 'Test2', 13.00, 989, 'Category2', 'Testing categories', 12);
 
 -- --------------------------------------------------------
 
@@ -202,6 +198,7 @@ CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `role` enum('user','admin') DEFAULT 'user',
   `status` enum('active','inactive') DEFAULT 'active',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
@@ -211,8 +208,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `role`, `status`, `created_at`) VALUES
-(1, 'Aatish', 'a@example.com', 'password', 'user', 'active', '2025-06-27 04:42:45');
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `phone`, `role`, `status`, `created_at`) VALUES
+(1, 'Aatish', 'atish@example.com', 'password', '9841693432', 'user', 'active', '2025-06-27 04:42:45');
 
 -- --------------------------------------------------------
 
@@ -230,7 +227,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `order_details`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `order_details`  AS SELECT `o`.`uid` AS `uid`, `o`.`order_id` AS `order_id`, `oi`.`order_item_id` AS `order_item_id`, `oi`.`product_id` AS `product_id`, `p`.`product_name` AS `product_name`, `i`.`image` AS `image`, `oi`.`price` AS `price`, `oi`.`quantity` AS `quantity`, `o`.`total` AS `total` FROM (((`orders` `o` join `order_items` `oi` on(`o`.`order_id` = `oi`.`order_id`)) join `products` `p` on(`p`.`product_id` = `oi`.`product_id`)) join `image` `i` on(`i`.`img_id` = `p`.`img_id`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `order_details`  AS SELECT `o`.`user_id` AS `user_id`, `o`.`order_id` AS `order_id`, `oi`.`order_item_id` AS `order_item_id`, `oi`.`product_id` AS `product_id`, `p`.`product_name` AS `product_name`, `i`.`image` AS `image`, `oi`.`order_price` AS `order_price`, `oi`.`quantity` AS `quantity`, `oi`.`sub_total` AS `sub_total` FROM (((`orders` `o` join `order_items` `oi` on(`o`.`order_id` = `oi`.`order_id`)) join `products` `p` on(`p`.`product_id` = `oi`.`product_id`)) join `image` `i` on(`i`.`img_id` = `p`.`img_id`)) ;
 
 --
 -- Indexes for dumped tables
@@ -254,8 +251,7 @@ ALTER TABLE `image`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `user_id` (`uid`);
+  ADD PRIMARY KEY (`order_id`);
 
 --
 -- Indexes for table `order_items`
@@ -285,7 +281,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `image`
@@ -297,13 +293,13 @@ ALTER TABLE `image`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
 
 --
 -- AUTO_INCREMENT for table `products`
